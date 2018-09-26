b44f21b77d17f8c3755ef71bd57ee04b3628f8a7
Anna Zamoriy <zamoriy.ania@gmail.com>
2018-09-26 18:49:21 +0300

    Create names.js

---
+++ names.js
@@ -0,0 +1,60 @@
+function childrenNames() {
+    function MakeRequest(query) {
+        var serverUrl = Xrm.Page.context.getClientUrl(); // My organization root url 
+        var oDataEndpointUrl = serverUrl + "/XRMServices/2011/OrganizationData.svc/"; // To locate and retrieve data 
+        oDataEndpointUrl += query;
+        var service = GetRequestObject();
+
+        if (service != null) {
+            service.open("GET", oDataEndpointUrl, false);
+            service.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+            service.setRequestHeader("Accept", "application/json, text/javascript, */*");
+            service.send(null);
+            var retrieved = JSON.parse(service.responseText).d;
+            var results = new Array();
+            for (var i = 0; i < retrieved.results.length; i++) {
+                results.push(retrieved.results[i]);
+            }
+            return results;
+        }
+        return null;
+    }
+
+    function GetRequestObject() {
+        if (window.XMLHttpRequest) {
+            return new window.XMLHttpRequest;
+        } else {
+            try {
+                return new ActiveXObject("MSXML2.XMLHTTP.3.0");
+            } catch (ex) {
+                return null;
+            }
+        }
+    }
+    console.log("HI!")
+
+    var parentId = Xrm.Page.data.entity.getId();
+    console.log(parentId);
+
+    SDK.REST.retrieveMultipleRecords("new_azamorii_2018_child", "$filter=new_Parent/Id eq guid'" + parentId + "'&$select=new_name", onSuccess, errorHandler, onComplete);
+    function onSuccess(result) {
+        result;
+
+        
+        for (var i = 0; i < result.length; i++) {                      
+            console.log(result[i]);
+        }
+
+        var childrenNamesField = Xrm.Page.getAttribute("new_childrennames").getValue();
+        console.log(childrenNamesField);
+       // Xrm.Page.getAttribute("new_childrennames").setValue();
+        console.log(result);
+    }
+    function errorHandler(error) {
+        alert(error.message);
+    }
+
+    function onComplete() {
+    }
+   
+}
